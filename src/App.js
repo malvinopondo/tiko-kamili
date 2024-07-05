b7babee733b3affeb3e4df94e0d5f5ed7109f974
Malvin Opondo <malvin.opondo@student.moringaschool.com>
2024-07-05 17:12:07 +0300

    feat(App.js): Set up my router inside the app.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import './App.css';
+import {Routes, Route} from 'react-router-dom';
 import Contact from "./Components/Contact";
 import Schedule from "./Components/Schedule";
+import Login from "./Components/Login"
+
 
 function App() {
   return (
     <div className="App">
      <Contact />
      <Schedule />
+     <Login />
     </div>
   );
 }
--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Login = () => {
   return (
-    <div>Login</div>
+    <h2>Login</h2>
   )
 }
 
