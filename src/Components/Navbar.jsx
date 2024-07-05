d180ae17999c2369b888f7d419ca55b7b448794a
Malvin Opondo <malvin.opondo@student.moringaschool.com>
2024-07-05 16:39:01 +0300

    feat(Navbar): Create a navbar component

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,9 +1,31 @@
 import React from 'react';
+import {Link} from 'react-router-dom'
+import {Navbar, Nav, Container, Button} from 'react-bootstrap';
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <Navbar.Brand as={Link} to="/">
+          TikoKamili
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav"></Navbar.Collapse>
+        <Nav className="me-auto">
+            <Nav.Link as={Link} to="/schedule">
+              Schedule
+            </Nav.Link>
+            <Nav.Link as={Link} to="/contact">
+              Contact
+            </Nav.Link>
+            </Nav>
+          <Nav>
+            <Button as={Link} to="/login" variant="outline-success">
+              Login
+            </Button>
+            </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
 
 export default Navbar;
